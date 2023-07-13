95454a1538d5f896d10177ad2e609020ffa0e8d1
JagannathShingne01 <jagannathshingne01@gmail.com>
2023-07-13 17:40:57 +0530

    Home-Page

---
+++ app.js
@@ -0,0 +1,30 @@
+<div class="content">
+<div class="user_name_time">
+    <h5>Jagannath Shingne <img src="./icon/bluetick.png" alt="" srcset=""><p>@mr.shingne</p></h5>
+    <h6><i class="fa-duotone fa-timer"></i>2 hours</h6>       
+</div>  
+<h3>Today on Diwali, Jaipur is decorated like a bride.</h3>   
+<div class="image_post_bx">
+    <img src="./img/img1.jpg" alt="" srcset="">
+    <img src="./img/img2.webp" alt="" srcset="">
+    <img src="./img/img3.jpg" alt="" srcset="">
+</div>
+<div class="tweet_card_soical_data">
+    <div class="tweet_soical_card">
+        <i class="fa-regular fa-comment"></i>
+        <span>128</span>
+    </div>
+    <div class="tweet_soical_card">
+        <i class="fa-solid fa-heart"></i>
+        <span>283</span>
+    </div>
+    <div class="tweet_soical_card">
+        <i class="fa-solid fa-retweet"></i>
+        <span>56</span>
+    </div>
+    <div class="tweet_soical_card">
+        <i class="fa-sharp fa-regular fa-share"></i>
+        <span></span>
+    </div>
+</div>    
+</div>
